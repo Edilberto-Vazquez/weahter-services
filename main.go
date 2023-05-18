2263f10396d2e5038b6e5698c83db2c16d40a949
Edilberto <edilberto.vazquez@lidz.mx>
2023-05-17 18:27:09 -0600

    chore: config created for load env vars

--- main.go
+++ main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/Edilberto-Vazquez/weahter-services/src/config"
+)
+
+func main() {
+	config.SetEnvironment()
+}
---
+++ src/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	ENVS = map[string]string{
+		"APP_ENV":  "",
+		"GIN_MODE": "",
+		"PORT":     "",
+		"DB_URI":   "",
+	}
+)
+
+func SetEnvironment() {
+
+	log.Println("Load environment vars")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	prefix := ""
+
+	switch os.Getenv("GIN_MODE") {
+	case "development":
+		prefix = "DEV_"
+	case "debug":
+		prefix = "DB_"
+	case "test":
+		prefix = "TEST_"
+	default:
+		prefix = ""
+	}
+
+	ENVS["APP_ENV"] = os.Getenv(prefix + "APP_ENV")
+	ENVS["PORT"] = os.Getenv(prefix + "PORT")
+	ENVS["DB_URI"] = os.Getenv(prefix + "DB_URI")
+
+	fmt.Println(ENVS)
+}
