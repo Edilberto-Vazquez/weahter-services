e54047f2a73fa3251394624f40ea973ed4aa712d
Edilberto <edilberto.vazquez@lidz.mx>
2023-05-18 21:57:03 -0600

    chore: se agregaron los campos Lightning y Distance al modelo de WeatherRecords

--- src/models/value_objects.go
+++ src/models/value_objects.go
@@ -17,16 +17,18 @@ type FindRecords struct {
 }
 
 type WeatherRecords struct {
-	DateTime time.Time `csv:"Fecha" bson:"datetime" json:"datetime"`
-	Temp     float64   `csv:"Temp" json:"temp"`
-	Chill    float64   `csv:"Chill" bson:"chill" json:"chill"`
-	Dew      float64   `csv:"Dew" bson:"dew" json:"dew"`
-	Heat     float64   `csv:"Heat" bson:"heat" json:"heat"`
-	Hum      float64   `csv:"Hum" bson:"hum" json:"hum"`
-	WspdAvg  float64   `csv:"Wspdavg" bson:"wspd_avg" json:"wspd_avg"`
-	WdirAvg  float64   `csv:"Wdiravg" bson:"wdir_avg" json:"wdir_avg"`
-	Bar      float64   `csv:"Bar" bson:"bar" json:"bar"`
-	Rain     float64   `csv:"Rain" bson:"rain" json:"rain"`
+	DateTime  time.Time `csv:"Fecha" bson:"datetime" json:"datetime"`
+	Lightning bool      `bson:"lightning" json:"lightning"`
+	Distance  uint8     `bson:"distance" json:"distance"`
+	Temp      float64   `csv:"Temp" json:"temp"`
+	Chill     float64   `csv:"Chill" bson:"chill" json:"chill"`
+	Dew       float64   `csv:"Dew" bson:"dew" json:"dew"`
+	Heat      float64   `csv:"Heat" bson:"heat" json:"heat"`
+	Hum       float64   `csv:"Hum" bson:"hum" json:"hum"`
+	WspdAvg   float64   `csv:"Wspdavg" bson:"wspd_avg" json:"wspd_avg"`
+	WdirAvg   float64   `csv:"Wdiravg" bson:"wdir_avg" json:"wdir_avg"`
+	Bar       float64   `csv:"Bar" bson:"bar" json:"bar"`
+	Rain      float64   `csv:"Rain" bson:"rain" json:"rain"`
 }
 
 type EFMElectricField struct {
